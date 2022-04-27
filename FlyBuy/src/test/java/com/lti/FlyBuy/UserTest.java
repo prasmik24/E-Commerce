6e7e3217e7b8d771c37dae7588c6c5c37a738d36
10672478 <10672478@LTILBOM20401034.lntinfotech.com>
2022-04-27 21:00:22 +0530

    Added all the Layers.

---
+++ FlyBuy/src/test/java/com/lti/FlyBuy/UserTest.java
@@ -0,0 +1,25 @@
+package com.lti.FlyBuy;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import com.lti.controller.UserController;
+import com.lti.dao.UserDaoImpl;
+import com.lti.model.User;
+
+class UserTest {
+
+	@Test
+	void test() {
+//		User user= new User();
+//		user.setUserName("Rohan");
+//		user.setUserEmail("rohan.997singh@gmail.com");
+//		user.setUserPassword("asdfghjkl");
+//		user.setUserMobile("7003551506");
+//		
+//		UserDaoImpl dao = new UserDaoImpl();
+//		dao.addUserWithCart(user);
+	}
+
+}
